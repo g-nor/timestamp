0acc2fb4eec87c344fdab8a3d65ccc156fd20415
Nor <g.nor.714@gmail.com>
2017-05-24 00:07:35 +1000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Timestamp microservice
+
+Input a date or a unix timestamp
+--------------------------------
+```
+https://[yourdomain]/May%2001,%202017
+```
+OR 
+```
+https://[yourdomain]/1493596800
+```
+
+Example output:
+```
+{ "unix": 1493596800, "natural": "May 01, 2017" }
+```
---
+++ server.js
@@ -0,0 +1,56 @@
+let express = require("express");
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+let app = express();
+
+app.get("/:date", (req, res) => {
+  let result = { unix: null, natural: null };
+  let d = null;
+  // check if its unix 
+  if(req.params.date == +req.params.date) {
+      // unix
+      d = new Date((+req.params.date)*1000);
+  } else {
+      // natural
+    d = new Date(req.params.date);
+  }
+
+  if (Object.prototype.toString.call(d) === "[object Date]") {
+    // it is a date
+    let unixtime = d.getTime();
+    if (!isNaN(unixtime)) {
+      // date is valid
+      result.unix = unixtime/1000;
+      result.natural = months[d.getMonth()] + " " + d.getDate() + ", " + d.getFullYear();
+    }
+  }
+  res.send(result);
+});
+
+app.get("/", (req, res) => {
+    let html = "<!DOCTYPE html><html>" +
+            "<head><title>Timestamp Microservice</title></head>" +
+            "<body><h1>Timestamp microservice<h1>" +
+            "<h4>Input format</h4><ul> <li>https://" +
+            req.headers.host +"/May%201,%202017</li><li>https://" +
+            req.headers.host +"/1493596800</li></ul>" +
+            "<h4>Output format</h4>" +
+            "<p>{\"unix\":1493596800,\"natural\":\"May 1, 2017\"}</p>"
+            "</body></html>";
+    res.send(html);
+})
+app.listen(8080);
